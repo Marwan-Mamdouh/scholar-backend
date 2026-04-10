45f41c334666bd7c60eead142528800800d3858d
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-04-10 03:54:57 +0200

    feat: add paginated response class to be our stranded page response

---
+++ src/utils/paginated.response.ts
@@ -0,0 +1,11 @@
+class PaginatedResponse<T> {
+	constructor(
+		public data: T[],
+		public total: number,
+		public page: number,
+		public hasPreviousPage: boolean,
+		public hasNextPage: boolean,
+	) {}
+}
+
+module.exports = PaginatedResponse;
