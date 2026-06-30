a5aa091493fe5f10d45e3e728cc7f378c8e7915d
YoussefOssamaa <y.ossama45@gmail.com>
2026-06-30 22:56:52 +0300

    Fix the building issue

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,16 +10,10 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 export default defineConfig({
     schema: "./src/db/prisma",
-    enums: "./src/db/prisma/enums",
     migrations: {
         path: "./src/db/prisma/migrations",
     },
     datasource: {
         url: env("DATABASE_URL"),
-    },
-    generator: {
-        client: {
-            provider: "prisma-client-js",
-        },
-    },
+    }
 });
