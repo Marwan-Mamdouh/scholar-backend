abe233123c67525b76cb422793dd6afdeb618a50
MohGendy <midoomidoooo12@gmail.com>
2026-07-12 13:24:35 +0300

    deprecate isAdmin function

--- src/index.js
+++ src/index.js
@@ -107,25 +107,6 @@ app.use((req, res, next) => {
     next();
 });
 
-const isAdmin = (req, res, next) => {
-    const token = req.cookies.auth_token; // tokens from cookies
-
-    if (!token) {
-        return res.status(401).json({ error: "Access Denied. No token provided." });
-    }
-
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET);
-        if (decoded.role !== 'admin') {
-            return res.status(403).json({ error: "Access Denied. Admins only." });
-        }
-        req.user = decoded; // save if needed
-        next(); // pass
-    } catch (ex) {
-        res.status(400).json({ error: "Invalid token." });
-    }
-};
-
 // --- FRONTEND ROUTES (HTML PAGES) ---
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
