3dc9112f193cb8ec90ad8a8366c00cd13e593d67
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-07-17 17:05:07 +0300

    refactor: export default db client to match what we have in the publication module

--- src/db/db_config.ts
+++ src/db/db_config.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-
 
 export const connectionString = process.env.DATABASE_URL;
 if (!connectionString) throw new Error("DATABASE_URL is required");
 export const db_adapter = new PrismaPg({ connectionString });
 export const db_client = new PrismaClient({ adapter: db_adapter });
 
+export default db_client;
