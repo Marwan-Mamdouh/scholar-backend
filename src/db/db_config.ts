d6615bce4cb3da1631f9a9ff9e655ba90020a864
YoussefOssamaa <y.ossama45@gmail.com>
2026-07-24 01:01:47 +0300

    Some cleaning to index.js

--- src/db/db_config.ts
+++ src/db/db_config.ts
@@ -7,5 +7,3 @@ export const connectionString = process.env.DATABASE_URL;
 if (!connectionString) throw new Error("DATABASE_URL is required");
 export const db_adapter = new PrismaPg({ connectionString });
 export const db_client = new PrismaClient({ adapter: db_adapter });
-
-export default db_client;
