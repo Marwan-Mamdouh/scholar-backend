90cf4eb503852e8dabf58a9d6aa02a0c5e1a257b
MohGendy <midoomidoooo12@gmail.com>
2026-09-08 01:39:30 +0300

    feat: add user CV parsing functionality with new routes and service

--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ import feedbackRouter from "./modules/feedback/feedback.routes.js";
 import researchersRouter from "./modules/researchers/researchers.routes.js";
 import teamRouter from "./modules/team/team.routes.js";
 import publicationsRouter from "./modules/publication/publication.routes.js";
+import usersRouter from "./modules/users/users.routes.ts";
 import { extractTopField, extractData } from "./utils/extractors.js";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/authentication/auth.js";
@@ -59,6 +60,7 @@ app.use("/api", researchersRouter);
 app.use("/api/publication", publicationsRouter);
 app.use("/api/team", teamRouter);
 app.use("/api/feedback", feedbackRouter);
+app.use("/api/users", usersRouter);
 
 // ================================================================
 //  SECTION: ACADEMIC SCANNER API (Researcher Analysis)
