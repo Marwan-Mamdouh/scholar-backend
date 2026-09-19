207f9a7114a20d1ba68a6db31dba49df2a68a660
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-09-19 17:41:28 +0300

    refactor(async-handler): use generic signatures for custom request types
    - Add generic <TReq extends Request = Request, TRes extends Response = Response> parameters
    - Allow custom request types (e.g. TypedRequest, PaginatedRequest) to be inferred cleanly
    - Tighten return type to Promise<unknown> | unknown
    - Restore tab indentation and POSIX trailing newline

--- src/lib/async.handler.ts
+++ src/lib/async.handler.ts
@@ -1,8 +1,14 @@
 import type { Request, Response, NextFunction, RequestHandler } from "express";
 
 const asyncHandler =
-  (fn: (req: Request, res: Response, next: NextFunction) => Promise<any> | any): RequestHandler =>
-  (req: Request, res: Response, next: NextFunction) =>
-    Promise.resolve(fn(req, res, next)).catch(next);
+	<
+		TReq extends Request = Request,
+		TRes extends Response = Response,
+	>(
+		fn: (req: TReq, res: TRes, next: NextFunction) => Promise<unknown> | unknown,
+	): RequestHandler =>
+	(req: Request, res: Response, next: NextFunction) =>
+		Promise.resolve(fn(req as TReq, res as TRes, next)).catch(next);
 
 export default asyncHandler;
+
